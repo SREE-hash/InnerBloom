462608956a2f2ce6e4d1e2152704ef575a5403c2
SREE-hash <sreejakolay376@gmail.com>
2026-09-20 11:03:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,6 +50,5 @@ def chat_with_memory(request: chatRequest):
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=10000)
-
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
  
